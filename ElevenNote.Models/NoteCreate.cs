f35d4a7868e167149a97812ce116373182d30e74
Ryan <Dibzthedibz@gmail.com>
2021-05-28 07:55:26 -0700

    Huge Commite. Back to PreCrash status.  Everything up through 4.0 modules completed and tested.

---
+++ ElevenNote.Models/NoteCreate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+    public class NoteCreate
+    {
+        [Required]
+        [MinLength(2, ErrorMessage = "Title must be longer than 2 characters.")]
+        [MaxLength(100, ErrorMessage = "Title Too Long.")]
+        public string Title { get; set; }
+        [MaxLength(8000)]
+        public string Content { get; set; }
+    }
+}
